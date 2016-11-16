055d58409e0dbbb80db4e05494a876c861144d06
Gonzalo Guevara <gonzarock@yahoo.com>
2016-11-16 13:39:37 -0300

    Añadido comentarios, no resetea cuando se hace uno

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import ComentarioForm
+from django import http
 
 # Create your views here.
 def post_list(request):
@@ -9,4 +11,16 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+
+    #cometarios
+    comentarios = post.comentarios.filter(activo=True)
+    if request.method == 'POST':
+        cometario_form = ComentarioForm(data=request.POST)
+        if cometario_form.is_valid():
+            nuevo_comentario = cometario_form.save(commit=False)
+            nuevo_comentario.post = post
+            nuevo_comentario.save()
+    else:
+        cometario_form = ComentarioForm()
+
+    return render(request, 'blog/post_detail.html', {'post': post, 'comentarios': comentarios, 'cometario_form': cometario_form})
